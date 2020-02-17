a8fb32aaf05d5c99af70969eec5b420d79201a84
picke122 <picke122@umn.edu>
2020-02-17 17:50:10 -0600

    new genres api

--- api/app.js
+++ api/app.js
@@ -7,6 +7,7 @@ var cors = require("cors");
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var randomMovieRouter = require('./routes/randomMovie');
+var genresRouter = require('./routes/genres');
 
 var app = express();
 
@@ -24,6 +25,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/randomMovie', randomMovieRouter);
+app.use('/genres', genresRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {

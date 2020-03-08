be94ebb6ead9da302b284615413b24dd4a62cb1b
Stuart Pickering <picke122@umn.edu>
2020-03-07 23:13:59 -0600

    randomMovie api v2

--- api/routes/randomMovie.js
+++ api/routes/randomMovie.js
@@ -1,11 +1,25 @@
 var express = require("express");
 var router = express.Router();
+let DateGenerator = require('random-date-generator');
 
 router.get("/", function(req, res, next) {
-    let movies = ['Lord of the Rings', 'Die Hard 2', 'V for Vendetta',
-                  'WALL-E', 'iRobot', 'Pirates of the Carribean']
-
-    res.send(movies[Math.floor(Math.random() * movies.length)]);
+    let startDate = new Date(1878, 6, 14);
+    let todaysDate = new Date
+    let endDate = new Date(todaysDate.getUTCFullYear(), todaysDate.getUTCMonth(), todaysDate.getUTCDate())
+    var request = require('request');
+    let randomDate = DateGenerator.getRandomDateInRange(startDate, endDate).toISOString().split('T')[0];
+    request('https://api.themoviedb.org/3/discover/movie?api_key=9b9ffe51ec80e0eb9192ace2d1c91a91&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&release_date.gte=' + randomDate + '&release_date.lte=' + randomDate, function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+            let jsonBody = JSON.parse(body)
+            if(jsonBody.results.length > 0){
+                let movieTitle = jsonBody.results[0].title
+                res.send(movieTitle);
+            }
+            else{
+                res.send("AHHH nothing found!!!")
+            }
+        }
+    })
 });
 
 module.exports = router;
